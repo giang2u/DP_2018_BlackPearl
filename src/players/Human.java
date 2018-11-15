5c186e832c9fbf9b28103495ee11568ec6a81a6e
Hue Nam <huenam.ly@gmail.com>
2018-11-15 14:44:42 +0100

    contruite les grille

--- src/players/Human.java
+++ src/players/Human.java
@@ -1,10 +1,13 @@
 package players;
 
 public class Human extends Player {
+	
 
 	public Human(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
 }
--- src/players/Player.java
+++ src/players/Player.java
@@ -7,10 +7,29 @@ public abstract class Player {
 	protected int [][] shipGrill = new int[SIZE][SIZE];
 	protected int [][] historyGrill = new int[SIZE][SIZE];
 	protected String playerName ="";
-	protected static int nbTireToucher = 0;
-	protected static int nbTireMiss = 0;
+	protected int nbTireToucher = 0;
+	protected int nbTireMiss = 0;
 	
 	public Player(String name){
 		this.playerName = name;
+		initGrill();
 	}
+	
+	public void toucher(){
+		this.nbTireToucher++;
+	}
+	
+	public void rater(){
+		this.nbTireMiss++;
+	}
+	
+	protected void initGrill(){
+		for(int i = 0 ; i < SIZE; i++){
+			for(int j = 0; j > SIZE;j++){
+				shipGrill[i][j] = 0;
+				historyGrill[i][j] = 0;
+			}
+		}
+	}
+	
 }
