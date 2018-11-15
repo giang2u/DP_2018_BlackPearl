c5b3882c7577968f540e15900f2d4da5e8a40047
giang2u <andre.giang8@etu.univ-lorraine.fr>
2018-11-15 14:20:33 +0100

    factory et ship squelette

---
+++ src/model/CenturyXX.java
@@ -0,0 +1,13 @@
+package model;
+
+public class CenturyXX extends EpochFactory {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
